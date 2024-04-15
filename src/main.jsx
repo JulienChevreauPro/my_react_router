c753b5a428ad0a1631da0dc5efa69ff9f4110b6a
JulienChevreauPro <julienchevreau33@gmail.com>
2024-04-15 11:46:18 +0200

    fixed about path

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx';
 import './index.css';
 
-import Home from "./pages/Home";
-import About from "./pages/About";
+import Home from "./pages/Home.jsx";
+import About from "./pages/about.jsx";
+import Article from './pages/Articles.jsx';
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
       {
         path:"/About",
         element:<About/>,
+      },
+      {
+        path:"/articles/:id",
+        element:<Article/>,
       },
     ],
   },
